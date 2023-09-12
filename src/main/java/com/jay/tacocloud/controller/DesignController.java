a276efa6fe9d86013114abb7f6b8fd3429b6178b
zpjayyy <jay.lichzp@gmail.com>
2023-09-12 22:27:13 +0800

    feat: design and order

--- src/main/java/com/jay/tacocloud/controller/DesignController.java
+++ src/main/java/com/jay/tacocloud/controller/DesignController.java
@@ -7,10 +7,7 @@ import com.jay.tacocloud.enums.Type;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -60,5 +57,12 @@ public class DesignController {
     @GetMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
     }
 }
---
+++ src/main/java/com/jay/tacocloud/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.jay.tacocloud.controller;
+
+import com.jay.tacocloud.entity.TacoOrder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:";
+    }
+}
